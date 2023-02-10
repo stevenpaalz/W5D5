e54f3d832f75874b37b0bc03c1107a9cf3d8a1eb
Steven Paalz <stevenpaalz@gmail.com>
2023-02-10 15:09:50 -0500

    make biggest cast

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
-
+  Movie.where(score: 2..3).select('id, title, score')
 end
 
 def harrison_ford
@@ -21,6 +21,7 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
 
+  Movie.joins(:actors).where("actors.name = 'Harrison Ford'").where.not("castings.ord = 1").select('movies.id, movies.title')
 end
 
 def biggest_cast
@@ -38,6 +39,14 @@ def biggest_cast
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
   
+  Movie
+    .joins(:castings)
+    .group('title', 'id')
+    # .having("COUNT(?) AS num_actors", "castings.actor_id")
+    # .count("castings.actor_id AS num_actors")
+    .order('COUNT(castings.actor_id) DESC')
+    .select(:id, :title)
+    .limit(3)
 end
 
 def directed_by_one_of(them)
